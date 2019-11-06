b719f427eecde80c81384a75373610077d92ecc0
Kesing Huang <kesing.huang@gmail.com>
2019-11-06 13:03:00 +0100

    editItem completed

--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -1,23 +1,57 @@
 import React, { Component } from 'react';
 
 class TodoItem extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.handleRemove= this.handleRemove.bind(this);
 
+        this.state = {
+            isEditing: false,
+            item: this.props.item
+        }
+        this.handleRemove = this.handleRemove.bind(this);
+        this.toggleEdit = this.toggleEdit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleUpdate = this.handleUpdate.bind(this);
     }
-    handleRemove(){
+    handleRemove() {
         this.props.remove(this.props.id);
     }
+    toggleEdit() {
+        //this.props.edit(this.props.id)
+        this.setState({ isEditing: !this.state.isEditing })
+    }
+    handleChange(e) {
+        this.setState({ item: e.target.value })
+    }
+    handleUpdate(e) {
+        e.preventDefault();
+        this.props.update(this.props.id, this.state.item)
+        this.setState({isEditing: false})
+    }
     render() {
-        
-        return (
-            <div>
-                <button>Edit</button>
-                <button onClick={this.handleRemove}>X</button>
-                <li>{this.props.item}</li>
-            </div>
-        )
+        let result;
+        if (this.state.isEditing) {
+            result = (
+                <div>
+                    <form onSubmit={this.handleUpdate}>
+                        <input value={this.state.item} onChange={this.handleChange} type='text' />
+                        <button>Save</button>
+                    </form>
+                </div>
+            )
+        } else {
+            result = (
+                <div>
+                    <button onClick={this.toggleEdit}>Edit</button>
+                    <button onClick={this.handleRemove}>X</button>
+                    <li>{this.props.item}</li>
+                </div>
+            )
+
+        }
+
+        return result;
+
     }
 }
 
--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import TodoForm from './TodoForm';
 import TodoItem from './TodoItem';
-//import uuid from 'uuid';
+
 
 class TodoList extends React.Component {
     constructor() {
         super();
         this.state = {
-            todos: [ ]
+            todos: []
         };
         this.addItem = this.addItem.bind(this);
         this.removeItem = this.removeItem.bind(this);
-
+        this.updateItem = this.updateItem.bind(this);
     }
     addItem(newItem) {
         this.setState({ todos: [...this.state.todos, newItem] })
@@ -22,16 +22,26 @@ class TodoList extends React.Component {
             todos: this.state.todos.filter(t => t.id !== id)
         })
     }
+    updateItem(id, updateItem) {
+        const updateTodos = this.state.todos.map(todo => {
+            if (todo.id === id) {
+
+                return { ...todo, item: updateItem }
+            }
+            return todo;
+        });
+        this.setState({todos: updateTodos})
+    }
     render() {
         const todos = this.state.todos.map(todoItem => {
-            return <TodoItem 
-            key = {todoItem.id} 
-            item = {todoItem.item}
-            remove = {this.removeItem}
-            id = {todoItem.id}
+            return <TodoItem
+                key={todoItem.id}
+                item={todoItem.item}
+                id={todoItem.id}
+                remove={this.removeItem}
+                update={this.updateItem}
             />
         });
-        //const wholeItem = this.state.todos
         return (
             <div>
                 <h1>What you wanna do?</h1>
