595257ed3a7f6cca7e6d7554285e7e4faeb9b8a2
Kesing Huang <kesing.huang@gmail.com>
2019-11-06 14:29:48 +0100

    express try 1

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require('path');
+const publicPath = path.join(__dirname, '..', 'public');
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+ });
+//Basically, we pass in all of the pieces of the path, 
+//and path.join puts them together. 
+//The result is then passed into app.use(express.static()), 
+//so Express will know which files to serve.
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}!`);
+ });
+ 
